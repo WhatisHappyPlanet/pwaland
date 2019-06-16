431f1110ac0316fdc08823bfe9c956ce3d04768e
brandonxiang <1542453460@qq.com>
2019-06-16 21:21:00 +0800

    feat: create pwa.json for data source

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
     title: `PWALAND`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    description: `A collection of pwa website`,
+    author: `@brandonxiang`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -27,8 +27,15 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    `gatsby-transformer-json`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `./data/`,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
   ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,51 @@
 import React from "react"
-import { Link } from "gatsby"
+// import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
+// import Image from "../components/image"
 import SEO from "../components/seo"
+// import Img from "gatsby-image"
+import { useStaticQuery, graphql } from "gatsby"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+const IndexPage = () => {
+
+  const { allPwaJson } = useStaticQuery(
+    graphql`
+      query {
+        allPwaJson {
+          edges {
+            node {
+              title
+              link
+              icon
+            }
+          }
+        }
+      }
+    `
+  )
+
+
+  const edges = allPwaJson.edges
+
+  console.log(edges)
+
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <section className="wrap">
+        {
+          edges.map(({node}, index) => (
+            <a href={node.link} className="box" key={index}>
+              <img src={node.icon} alt={node.title}/>
+              <span>{node.title}</span>
+            </a>
+          ))
+        }
+
+      </section>
+    </Layout>
+  )
+} 
 
 export default IndexPage
